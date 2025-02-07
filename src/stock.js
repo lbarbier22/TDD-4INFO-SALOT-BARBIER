be265a40dc0dc20206f902678599594a2ee13338
Emma Salot <emma.salot.etu@univ-lille.fr>
2025-02-07 11:43:08 +0100

    feat : display warehouse function

--- src/stock.js
+++ src/stock.js
@@ -1,5 +1,11 @@
-function display(warehouse){
-    throw new Error("not implemented");
+function display(warehouse) {
+    warehouse.forEach(article => {
+        let res = article;
+        if (article.quantity < 50) {
+            res += " flag";
+        }
+        console.log(res);
+    });
 }
 
 module.exports = {display};
